ae9131b8e9d11b8f57bc7d8718a018b5911ba1c9
Caterina Magri <cat.magri@gmail.com>
2020-09-11 15:11:55 -0400

    added functions

---
+++ feature_extractor.py
@@ -0,0 +1,40 @@
+from torch import nn
+from torchvision.models.alexnet import alexnet
+
+## Specify Alexnet model
+class AlexNet(nn.Module):
+    def __init__(self, feature_name):
+        super().__init__()
+        self.feature_name = feature_name
+        base = alexnet(pretrained=True)
+        self.conv_1 = base.features[:3]
+        self.conv_2 = base.features[3:6]
+        self.conv_3 = base.features[6:8]
+        self.conv_4 = base.features[8:10]
+        self.conv_5 = base.features[10:]
+        self.avgpool = base.avgpool
+        self.fc_1 = base.classifier[:3]
+        self.fc_2 = base.classifier[3:6]
+        self.fc_3 = base.classifier[6:]
+        self.eval()
+    def forward(self, stimuli):
+        x = self.conv_1(stimuli)
+        if 'conv_1' == self.feature_name: return x.view(x.shape[0], -1)
+        x = self.conv_2(x)
+        if 'conv_2' == self.feature_name: return x.view(x.shape[0], -1)
+        x = self.conv_3(x)
+        if 'conv_3' == self.feature_name: return x.view(x.shape[0], -1)
+        x = self.conv_4(x)
+        if 'conv_4' == self.feature_name: return x.view(x.shape[0], -1)
+        x = self.conv_5(x)
+        if 'conv_5' == self.feature_name: return x.view(x.shape[0], -1)
+        x = self.avgpool(x)
+        x = x.view(x.shape[0], -1)
+        if 'pool' == self.feature_name: return x
+        x = self.fc_1(x)
+        if 'fc_1' == self.feature_name: return x
+        x = self.fc_2(x)
+        if 'fc_2' == self.feature_name: return x
+        x = self.fc_3(x)
+        if 'fc_3' == self.feature_name: return x
+        return None 
--- utils.py
+++ utils.py
@@ -1,47 +1,6 @@
-
-from torch import nn
-import torch
-from torchvision.models.alexnet import alexnet
 import os
-
-
-## Specify Alexnet model
-class AlexNet(nn.Module):
-    def __init__(self, feature_name):
-        super().__init__()
-        self.feature_name = feature_name
-        base = alexnet(pretrained=True)
-        self.conv_1 = base.features[:3]
-        self.conv_2 = base.features[3:6]
-        self.conv_3 = base.features[6:8]
-        self.conv_4 = base.features[8:10]
-        self.conv_5 = base.features[10:]
-        self.avgpool = base.avgpool
-        self.fc_1 = base.classifier[:3]
-        self.fc_2 = base.classifier[3:6]
-        self.fc_3 = base.classifier[6:]
-        self.eval()
-    def forward(self, stimuli):
-        x = self.conv_1(stimuli)
-        if 'conv_1' == self.feature_name: return x.view(x.shape[0], -1)
-        x = self.conv_2(x)
-        if 'conv_2' == self.feature_name: return x.view(x.shape[0], -1)
-        x = self.conv_3(x)
-        if 'conv_3' == self.feature_name: return x.view(x.shape[0], -1)
-        x = self.conv_4(x)
-        if 'conv_4' == self.feature_name: return x.view(x.shape[0], -1)
-        x = self.conv_5(x)
-        if 'conv_5' == self.feature_name: return x.view(x.shape[0], -1)
-        x = self.avgpool(x)
-        x = x.view(x.shape[0], -1)
-        if 'pool' == self.feature_name: return x
-        x = self.fc_1(x)
-        if 'fc_1' == self.feature_name: return x
-        x = self.fc_2(x)
-        if 'fc_2' == self.feature_name: return x
-        x = self.fc_3(x)
-        if 'fc_3' == self.feature_name: return x
-        return None 
+from torchvision.transforms import functional as tr
+from PIL import Image
 
 
 
@@ -52,3 +11,19 @@ def listdir(dir, path=True):
     if path:
         files = [os.path.join(dir, f) for f in files]
     return files
+
+
+def image_to_tensor(image, resolution=None, do_imagenet_norm=True):
+    if isinstance(image, str):
+        image = Image.open(image).convert('RGB')
+    if resolution is not None:
+        image = tr.resize(image, resolution)
+    if image.width != image.height:     # if not square image, crop the long side's edges
+        r = min(image.width, image.height)
+        image = tr.center_crop(image, (r, r))
+    image = tr.to_tensor(image)
+    if do_imagenet_norm:
+        image = imagenet_norm(image)
+    return image
+
+

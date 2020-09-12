e7086a3b5f6dc13eb79acf80e76c644390ac8a8d
Caterina Magri <cat.magri@gmail.com>
2020-09-12 16:35:28 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,8 +5,13 @@ from PIL import Image
 import torch
 from tqdm import tqdm
 
+# ImageNet mean and standard deviation. All images
+# passed to a PyTorch pre-trained model (e.g. AlexNet) must be
+# normalized by these quantities, because that is how they were trained.
+imagenet_mean = (0.485, 0.456, 0.406)
+imagenet_std = (0.229, 0.224, 0.225)
 
-def compute_features(model, conditionsPath):
+def compute_features(model, conditionsPath, resolutionval):
     #takes model and loads the features for that image, needs path to of files in directory
     conditions = listdir(conditionsPath)
     condition_features = {}
@@ -34,17 +39,29 @@ def listdir(dir, path=True):
     return files
 
 
-def image_to_tensor(image, resolution=None, do_imagenet_norm=True):
+def image_to_tensor(image, resolution=None,padding=None, do_imagenet_norm=True):
     if isinstance(image, str):
         image = Image.open(image).convert('RGB')
-    if resolution is not None:
-        image = tr.resize(image, resolution)
-    if image.width != image.height:     # if not square image, crop the long side's edges
+     if image.width != image.height:     # if not square image, crop the long side's edges to make it square
         r = min(image.width, image.height)
         image = tr.center_crop(image, (r, r))
+    if padding:     # if not square image, crop the long side's edges to make it square
+        image = tr.pad(input=data, pad=(padding, padding, padding, padding), mode='constant', value=0)
+    if resolution is not None:#f size is an int, smaller edge of the image will be matched to this number
+        image = tr.resize(image, resolution) 
     image = tr.to_tensor(image)
     if do_imagenet_norm:
         image = imagenet_norm(image)
+    return image
+
+def imagenet_norm(image):
+    dims = len(image.shape)
+    if dims < 4:
+        image = [image]
+    image = [tr.normalize(img, mean=imagenet_mean, std=imagenet_std) for img in image]
+    image = torch.stack(image, dim=0)
+    if dims < 4:
+        image = image.squeeze(0)
     return image
 
 
